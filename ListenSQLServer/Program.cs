070125dee7bfd97f3366f8975bb227f22e9eb414
Huy Tran Phuoc <Huy.TranPhuoc@ebiquity.com>
2023-08-08 17:01:25 +0700

    Update

--- ListenSQLServer/Program.cs
+++ ListenSQLServer/Program.cs
@@ -1,18 +1,30 @@
 ﻿using System.Data.SqlClient;
 
-string connectionString = "Data Source=VNNOT02172\\SQLEXPRESS;Initial Catalog=Research;Integrated Security=True";
+string connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=Research;Integrated Security=True";
 SqlDependency.Start(connectionString);
-using var connection = new SqlConnection(connectionString);
-connection.Open();
-using var command = new SqlCommand("SELECT * FROM [Users]", connection);
-var dependency = new SqlDependency(command);
-dependency.OnChange += Dependency_OnChange;
-var reader = command.ExecuteReader();
+RegisterOnChangeEvent(connectionString);
 Console.ReadLine();
+
+static void RegisterOnChangeEvent(string connectionString)
+{
+    using var connection = new SqlConnection(connectionString);
+    connection.Open();
+    using var command = new SqlCommand("SELECT [Name] FROM dbo.Users WHERE [Id]=9", connection);
+    var dependency = new SqlDependency(command);
+    dependency.OnChange += Dependency_OnChange;
+    var reader = command.ExecuteReader();
+}
 
 static void Dependency_OnChange(object sender, SqlNotificationEventArgs e)
 {
+    string connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=Research;Integrated Security=True";
     Console.WriteLine(e.Info);
     Console.WriteLine(e.Source);
     Console.WriteLine(e.Type);
+
+    if (sender is SqlDependency dependency)
+    {
+        dependency.OnChange -= Dependency_OnChange;
+        RegisterOnChangeEvent(connectionString) ;
+    }
 }
